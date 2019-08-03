39610c04677e6087fa0539e5c8fad3210b5f084e
ablinn91 <52763181+ablinn91@users.noreply.github.com>
2019-08-03 12:03:42 -0500

    Add files via upload

--- PyBank.py
+++ PyBank.py
@@ -3,7 +3,7 @@ import csv
 #import in the data
 budget_data = os.path.join("budget_data.csv")
 
-#find the variables
+#Assign needed variables
 number_month = 0
 total_profit_loss = 0
 delta_profit =0
@@ -28,31 +28,26 @@ with open(budget_data, newline='') as csv_file:
         #Finds the total profit for the problem
         total_profit_loss = x + total_profit_loss
         #finds total number of months by just ading up all the rows 
-        number_month = number_month +1
+        number_month = number_month + 1
         delta_profit = x - y
-        #gets rid of the first chagne becouse that would just be the first value
+        #gets rid of the first delta_profit becouse that would just be the first value
         if z==0:
-            delta_profit = delta_profit- delta_profit
+            delta_profit = delta_profit - delta_profit
             z = z+1
         total_delta_profit = delta_profit_total + delta_profit 
 
-        
         #finds greatest gain and a signs
         if delta_profit > greatest_gain:
             greatest_gain = delta_profit
             greatest_gain_month = row[0]
-        #finds greatest losss and signs
+        #finds greatest loss and signs
         if delta_profit < greatest_loss:
             greatest_loss = delta_profit
             greatest_loss_month = row[0]    
-
-
-
-        
         
         delta_profit_total = delta_profit + delta_profit_total
 
-        #set y to the curent row, which will be the last row
+        #set y to the curent row, which will be the last row in the next integration
         y = x
 
 #print out the data in the analysis.         
